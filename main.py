eb40189cfe731477b7f674b0f14f0b3df750b91f
bitmax-ux <awelalemu798@gmail.com>
2026-06-24 08:55:16 +0300

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,24 @@ import re
 import time
 import requests
 import telebot
+import os
+from flask import Flask
+from threading import Thread
+
+app = Flask('')
+
+@app.route('/')
+def home():
+    return "Bot is running!"
+
+def run():
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 8080)))
+
+def keep_alive():
+    t = Thread(target=run)
+    t.start()
+
+keep_alive()
 
 # የአንተ የቦት መረጃዎች
 BOT_TOKEN = "8887015953:AAHarP6TAfFbOUwic8OlnVBnmSobM9jRCzE"
